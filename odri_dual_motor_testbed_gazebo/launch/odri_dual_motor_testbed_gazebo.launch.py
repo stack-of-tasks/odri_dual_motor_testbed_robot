7cf56317f289ade9a882043f6121d65632efd97c
Olivier Stasse <olivier.stasse@laas.fr>
2026-06-21 22:32:58 +0200

    Use use_sim_time

--- odri_dual_motor_testbed_gazebo/launch/odri_dual_motor_testbed_gazebo.launch.py
+++ odri_dual_motor_testbed_gazebo/launch/odri_dual_motor_testbed_gazebo.launch.py
@@ -10,8 +10,15 @@ from launch.actions import (
     SetLaunchConfiguration,
 )
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+
 from launch.logging import get_logger
-from launch.substitutions import Command, FindExecutable, PathJoinSubstitution
+from launch.substitutions import (
+    Command,
+    FindExecutable,
+    LaunchConfiguration,
+    PathJoinSubstitution,
+)
+
 from launch_ros.actions import Node
 from launch_ros.parameter_descriptions import ParameterValue
 from launch_ros.substitutions import FindPackageShare
@@ -80,11 +87,21 @@ def generate_launch_description():
         "robot_description": ParameterValue(robot_description_content, value_type=str)
     }
 
+    set_sim_time = SetLaunchConfiguration("use_sim_time", "True")
+
+    clock_bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        name='clock_bridge',
+        output='screen',
+        arguments=['/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock'],
+    )
+
     node_robot_state_publisher = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
         output="screen",
-        parameters=[robot_description],
+        parameters=[robot_description, {"use_sim_time": LaunchConfiguration("use_sim_time")}],
     )
 
     robot_spawn = IncludeLaunchDescription(
@@ -103,29 +120,39 @@ def generate_launch_description():
         }.items(),
     )
 
+<<<<<<< HEAD
     spawn_controller = Node(
+=======
+    spawn_joint_state_broadcaster = Node(
+>>>>>>> eb6aaa6 (Use use_sim_time)
         package="controller_manager",
-        executable="spawner.py",
+        executable="spawner",
         arguments=["joint_state_broadcaster"],
+        parameters=[{"use_sim_time": LaunchConfiguration("use_sim_time")}],
         output="screen",
     )
 
-    spawn_controller_effort = Node(
+    spawn_odri_fcc = Node(
         package="controller_manager",
-        executable="spawner.py",
-        arguments=["effort_controllers"],
+        executable="spawner",
+        arguments=["odri_fcc"],
+        parameters=[{"use_sim_time": LaunchConfiguration("use_sim_time")}],
         output="screen",
     )
 
     print("gz_model_path_env_var;" + str(gz_model_path_env_var))
     return LaunchDescription(
         [
+            set_sim_time,
             gz_model_path_env_var,
             gz_sim_sys_plugin_path,
             gz_sim_server,
             gz_sim_client,
+            clock_bridge,
             node_robot_state_publisher,
             robot_spawn,
+            spawn_joint_state_broadcaster,
+            spawn_odri_fcc,
         ]
     )
 
